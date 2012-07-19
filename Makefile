d990383cbe138a3c0bcba9fabe1e6885d871ee1d
Bryan Lunt <lunt@ctbp.ucsd.edu>
2012-07-19 16:37:37 -0700

    more cleaning/modification

---
+++ Makefile
@@ -0,0 +1,14 @@
+prep:
+	python generate.py foo.faa DIs/long_200.DI hmmtop.out
+	cns_solve < cns_scripts/generate_seq.inp > cns_output/generate_seq.out
+	cns_solve < cns_scripts/generate_extended.inp > cns_output/generate_extended.out
+
+run:
+	cns_solve < cns_scripts/anneal.inp > cns_output/anneal.out
+	cns_solve < cns_scripts/accept.inp > cns_output/accept.out
+	cns_solve < cns_scripts/dg_sa.inp > cns_output/dg_sa.out
+
+all: clean prep run
+
+clean:
+	rm -f cns_output/* my* *.pdb
--- generate.py
+++ generate.py
@@ -3,9 +3,13 @@
 import scipy as S
 
 import sys
+from itertools import izip, count
 
 import Bio.SeqIO
 import Bio.PDB.Polypeptide as PP
+
+from SS_const import *
+
 
 INFILE = open(sys.argv[1])
 
@@ -17,23 +21,24 @@ seq = Bio.SeqIO.read(INFILE,'fasta')
 seqStr = seq.seq.tostring()
 seqLen = len(seqStr)
 
+
 outSeq = [PP.one_to_three(i) for i in seqStr]
 
 OUTSEQ = open('my.seq','w')
 
 for i in range(seqLen/10):
-	OUTSEQ.write(' '.join(outSeq[i:i+10]))
+	OUTSEQ.write(' '.join(outSeq[i*10:i*10+10]))
 	OUTSEQ.write('\n')
 OUTSEQ.close()
 
 
 #make DI constraints
-constraintstr = 'assign (resid %i and name CA)  (resid %i and name CA)  %.1f %.1f %.1f \n'
+constraintstr = 'assign (resid %i and name CA)  (resid %i and name CA)  %.1f %.1f %.1f weight %.5f\n'
 
 
 OUTCONS = open('my.tbl','w')
-for i,j,k in DI:
-	OUTCONS.write(constraintstr % (int(i),int(j), 7.0, 1.0,2.0))
+for (i,j,k),number in izip(DI,count(1)):
+	OUTCONS.write(constraintstr % (int(i),int(j), 7.0, 4.0, 3.0,10.0/number))
 OUTCONS.close()
 
 
@@ -44,21 +49,17 @@ for line in HMMTOPFILE:
 		TMstr = map(lambda x: tuple(map(int,x.split('-'))), line.split(':')[1].strip().split(' '))
 HMMTOPFILE.close()
 
+
+#actually make SS constraints
 OUTSS = open('my_SS.tbl','w')
 OUTSSA = open('my_SS_angle.tbl','w')
+
 for Hstart, Hend in TMstr:
-	for i in range(Hstart,Hend+1):
-		if i+1 <= Hend:
-			OUTSSA.write('assign (resid %i and name c) (resid %i and name n) (resid %i and name ca) (resid %i and name c)  5.0 -57.0 7.0 2\n' % (i, i+1, i+1, i+1))
-			OUTSSA.write('assign (resid %i and name n) (resid %i and name ca) (resid %i and name c) (resid %i and name n)  5.0 -47.0 7.0 2\n' % (i,i,i,i+1))
-			OUTSS.write(constraintstr % (i, i+1, 3.82, 0.2, 0.2))
-		if i+2 <= Hend:
-			OUTSS.write(constraintstr % (i, i+2, 5.5, 0.3, 0.3))
-		if i+3 <= Hend:
-			OUTSS.write(constraintstr % (i, i+3, 5.3, 0.65, 0.65))
-		if i+4 <= Hend:
-			OUTSS.write(constraintstr % (i, i+4, 6.35, 0.7, 0.7))
-		if i+5 <= Hend:
-			OUTSS.write(constraintstr % (i, i+5, 8.7, 0.6, 0.6))
+	thisTM_dists = make_SS_dists(Hstart,Hend-Hstart)
+	thisDihedral = make_helix_dihedral(Hstart,Hend-Hstart)
+
+	OUTSS.write(thisTM_dists)
+	OUTSSA.write(thisDihedral)
+
 OUTSS.close()
 OUTSSA.close()
