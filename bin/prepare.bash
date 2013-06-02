1015b1d401ed07666911c776c35a9a642df56d1e
Bryan Lunt <lunt@ctbp.ucsd.edu>
2013-06-02 14:49:15 -0700

    Better file naming.

---
+++ bin/prepare.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script will do the basic preparation for a run.
+#Predict the secondary structure,
+#Create the enlongated sequence,
+#Etc, Etc.
+#
+#Mostly, It wraps a python script.
+#
+
+FOOBAR
